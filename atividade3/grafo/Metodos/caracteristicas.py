fc67602a4a3645e5a7489b32501f0d8e1c96e241
Vitória Thaís <80798968+vitori4th@users.noreply.github.com>
2022-10-07 20:52:33 -0300

    Update

--- atividade3/grafo/Metodos/caracteristicas.py
+++ atividade3/grafo/Metodos/caracteristicas.py
@@ -1,26 +1,11 @@
-'''=================================================
-UNIVERSIDADE FEDERAL DE ITAJUBÁ
-INSTITUTO DE MATEMÁTICA E COMPUTAÇÃO
-SIN110 - ALGORITMOS E GRAFOS
-Prof. Rafael Frinhani
+import numpy
 
-caracteristicas - Funções para obtenção das características do grafo e operações em uma matriz de adjacências.
-
-05/09/2022
-===================================================='''
-
-import numpy as np
-
-'''Verifica Adjacência: Função que verifica se os vértices vi e vj são adjacentes.
-Entrada: matriz de adjacências (numpy.ndarray), vi (Integer), vj (Integer)
-Saída: 0 (Integer) se vi e vj NÃO são adjacentes; 1 se vi e vj são adjacentes'''
 def verificaAdjacencia(matriz, vi, vj):
-    if matriz[vi][vj] > 0: # Se célula M[vi][vj] for maior que 0 existe uma ou mais arestas
-        verticesAdjacentes = True
+    if matriz[vi][vj]> 0:
+        verificaAdjacentes = True
     else:
-        verticesAdjacentes = False
-    print('Vertices', vi, 'e', vj, 'são adjacentes?', verticesAdjacentes, '\n')
-    return verticesAdjacentes
+        verificaAdjacentes = False
+    return print(verificaAdjacentes)
 
 def tipoGrafo(matriz):
     laco = False 
@@ -29,34 +14,39 @@ def tipoGrafo(matriz):
     qtdVertices = np.shape(matriz)[0] #descobre dimensão da matriz
     for vi in range(0, qtdVertices):  # Para cada vértice vi
         if matriz[vi][vi]>=1:
-                    laco = True #verifica se possui laço
-        if matriz[vi][vi]>1:
-                    arestaMultipla = True #verifica se possui arestas multiplas
-        for vj in range(vi+1, qtdVertices): # Para cada vértice vj
+            laco = True #verifica se possui laço
+        for vj in range(0, qtdVertices): # Para cada vértice vj
             controle = matriz[vi][vj]
+            if matriz[vi][vj]>1:  #verifica se possui arestas multiplas
+                    arestaMultipla = True  
+                
             while controle > 0: # Adiciona a quantidade de arestas paralelas ou peso da aresta
                 if (matriz[vi][vj])!=(matriz[vj][vi]): #verifica se é um grafo direcionado
                     direcionado=True
                 if controle>1:  #verifica se possui arestas multiplas
                     arestaMultipla = True  
                 controle -= 1
-    if not laco and not arestaMultipla: #verifica se possui não tem arestas multiplas e nem laços: GRAFO SIMPLES
-        return 0 #simples 
-    if direcionado: #verifica se é direcionado: DÍGRAFO
-        return 1 #dígrafo
-    if not laco and arestaMultipla: #verifica se possui arestas multiplas e não possui laço: MULTIGRAFO
-        return 2 # multigrafo 
-    if laco and arestaMultipla: #verifica se não possui arestas multiplas e possui laço: PSEUDOGRAFO
-        return 3 #pseudografo
 
+    if not laco and not arestaMultipla and not direcionado: #verifica se não tem arestas multiplas e nem laços: GRAFO SIMPLES
+        return print(0) #simples 
+    if not laco and arestaMultipla and direcionado: #verifica se possui arestas multiplas e não possui laço: MULTIGRAFO
+        return print(21) # multigrafo dirigido
+    if not laco and arestaMultipla and not direcionado: #verifica se possui arestas multiplas e não possui laço: MULTIGRAFO
+        return print(20) # multigrafo 
+    if laco and not direcionado: #verifica se não possui arestas multiplas e possui laço: PSEUDOGRAFO
+        return print(30) #pseudografo
+        
+    if laco and direcionado : #verifica se não possui arestas multiplas e possui laço: PSEUDOGRAFO
+        return print(31) #pseudografo dirigido
+      
+    
+    if direcionado and not laco and not arestaMultipla: #verifica se é direcionado: DÍGRAFO
+        return print(1) #dígrafo
 def calcDensidade(matriz):
     densidade = 0
     qtdVertices = np.shape(matriz)[0] #descobre quantidade de vertices
     qtdArestas=0
-    x = tipoGrafo(matriz)
-    simples = x==0
-    digrafo = x==1
-
+  
     #calcula quantidade de arestas
     for vi in range(0, qtdVertices):  # Para cada vértice vi
         for vj in range(vi+1, qtdVertices): # Para cada vértice vj
@@ -65,50 +55,50 @@ def calcDensidade(matriz):
                 qtdArestas+=1
                 controle -= 1
 
-    if simples: #se grafo simples calcula densidade de um jeito 
-        densidade = (2 * qtdArestas)/(qtdVertices * (qtdVertices-1))
-    if digrafo:#se grafo digrafo calcula densidade de outro jeito 
-        densidade = qtdArestas/(qtdVertices  *(qtdVertices-1))
+    densidade = (2 * qtdArestas)/(qtdVertices * (qtdVertices-1))
+
     
     densidadeFormatada = "{:.3f}".format(densidade) #formata densidade parad tres casas decimais
-    return float(densidadeFormatada)
-
+    return print(float(densidadeFormatada))
 def insereAresta(matriz, vi, vj):
-    matriz[vi][vj] +=1 #soma um na posição(aresta) desejada
-    return matriz
-
-def insereVertice(matriz, vi):
+    matriz[vj][vi] +=1 #soma um na posição(aresta) desejada
+    matriz[vi][vj] +=1
+    return print(matriz)
+def insereVertice(matriz):
+    matriz = np.array(matriz)
     vi = [] #define vi como um array vazio
-    size=np.shape(matriz) #decobre denisdade da matriz
+    size=np.shape(matriz) #decobre tamanho da matriz
     x=size[0]
 
     #adiciona valores ao vetor
     while x>0:
-        vi.append(3) 
+        vi.append(0) 
         x-=1
     npArray=np.array(vi) #traforma um array em um array numpy
-
+    lastRow =matriz.shape[0]
     #adiciona linha a matriz numpy
-    matriz = np.insert(matriz,matriz.shape[0],[npArray],axis= 0)  
+    matriz = np.insert(matriz,lastRow,[npArray],axis= 0)  
     vi.append(0) #adicionando mais um zero para completar a coluna, já que foi inserido uma nova linha
 
     colunaAdicionada = np.array(vi)
     #adiciona coluna a matriz numpy
     matriz = np.column_stack((matriz, colunaAdicionada))
- 
    
-    return matriz
-
+    return print(matriz)
 def removeVertice(matriz, vi):
-    array = np.array(matriz) #define um array do tipo numpy 
 
-    array = np.delete(array,vi, axis = 0) #deleta linha do vertice inserida pelo usuário
-             
-    result = np.delete(array, vi, 1)  #deleta coluna do vertice inserida pelo usuário
+    x=0
+    size = len(matriz[vi])
+    while x< size:
+        matriz[vi][x]= -1 
+        matriz[x][vi]= -1 
+        x+=1
 
-    return result
-
+    m=np.array(matriz)
+    return print(m)
 def removeAresta(matriz, vi, vj):
     if matriz[vi][vj]>=1: #subtrai um na posição(aresta) desejada se existir aresta(maior que zero)
         matriz[vi][vj] -=1
-    return matriz
+    if matriz[vj][vi]>=1: #subtrai um na posição(aresta) desejada se existir aresta(maior que zero)
+        matriz[vj][vi] -=1
+    return print(matriz)
